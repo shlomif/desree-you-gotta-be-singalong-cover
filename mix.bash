18aec6b2e6e8481f7ef924fa9b3474e305b62e82
Shlomi Fish <shlomif@shlomifish.org>
2023-04-23 12:57:02 +0300

    add code

---
+++ mix.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+out="desree-you-gotta-be-singalong-cover.mp4"
+rm -f "$out"
+# ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
+# ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
+# ffmpeg -loop 1 -i 15526594522_ba001951b9_c.bmp -i tiff1.wav  -shortest -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
+# ffmpeg -f image2 -i 15526594522_ba001951b9_c.bmp -i tiffany-cover.wav -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
+ffmpeg -loop 1 -f image2 -i IMG_2341.JPG -i Recording_43.m4a -shortest -q:v 5 -s 800x600 "$out"
